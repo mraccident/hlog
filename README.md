c5c44b33dd5dcff7bac85a916ac0cf944cdf7c22
Trip Volpe <trip@flowroute.com>
2014-07-28 02:41:56 -0700

    Add basic TCP server and README

---
+++ README.md
@@ -0,0 +1,16 @@
+hlog
+====
+
+`hlog` is my from-the-ground-up blog. It is titled under the theory that
+software that is unpleasant to use should be unpleasant to pronounce. It is
+expected that further revisions of this software will add more consonants to
+the title; for example, `hglog` or `schplog`. This scheme is chosen for its
+clear advantages over semantic versioning.
+
+Features
+--------
+
+Currently, this blog implements just the most basic, fundamental feature of
+any blogging platform. On receipt of an HTTP GET request to "/" (or, for that
+matter, any TCP request of any kind whatsoever), it responds with the message
+"Piss off."
---
+++ hlog.hs
@@ -0,0 +1,36 @@
+import Network hiding (accept)
+import Network.Socket (fdSocket, accept)
+import Network.Socket.ByteString
+import Data.List (intercalate)
+import Data.ByteString.Char8 hiding (length, intercalate)
+import GHC.Event
+import System.Posix
+import System.Posix.IO
+
+main :: IO ()
+main = withSocketsDo $ do
+    sock <- listenOn $ PortNumber 1337
+    let fd = fromIntegral (fdSocket sock)
+    mgr <- new
+    registerFd mgr (client sock) fd evtRead
+    loop mgr
+
+client sock _ _ = do
+    (c, _) <- accept sock
+    sendAll c msg
+    sClose c
+
+httpify :: String -> String
+httpify content = 
+    intercalate "\r\n" [
+          "HTTP/1.0 200 OK"
+        , "Content-Length: " ++ (show $ length content)
+        , ""
+        , content
+        , ""]
+
+-- Feck off, standards
+html :: String -> String
+html body = "<html><pre>" ++ body
+
+msg = pack $ httpify $ html "Piss off."
