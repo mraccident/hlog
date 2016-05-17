b57bb3f6225ba35d7b87f45db343185a8bf3b084
Trip Volpe <trip.volpe@gmail.com>
2016-05-16 20:08:10 -0700

    Move server functions to src/Lib.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,39 +1,5 @@
-{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Prelude hiding (length, intercalate)
-import Data.ByteString.Char8
-import Network hiding (accept)
-import Network.Socket hiding (sClose)
-import Network.Socket.ByteString (sendAll)
-import Control.Concurrent
+import Lib
 
-main = withSocketsDo $ do
-    sock <- listenOn $ PortNumber 1337
-    loop sock
-
-loop sock = do
-    (conn, _) <- accept sock
-
-    -- Print incoming request information
-    req <- recv conn 4096
-    peer <- getPeerName conn
-    Prelude.putStrLn $ show peer ++ ": " ++ show req
-
-    forkIO $ body conn
-    loop sock
-    where
-        body c = do
-            sendAll c msg
-            sClose c
-
-httpify :: ByteString -> ByteString
-httpify content =
-    intercalate "\r\n" [
-          "HTTP/1.0 200 PISS OFF"
-        , "Content-Length: " `append` (pack $ show $ length content)
-        , ""
-        , content]
-
-msg :: ByteString
-msg = httpify "Piss off."
+main = serve 1337
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,42 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Lib
-    ( someFunc
+    ( serve
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Prelude hiding (length, intercalate)
+import Data.ByteString.Char8
+import Network hiding (accept)
+import Network.Socket hiding (sClose)
+import Network.Socket.ByteString (sendAll)
+import Control.Concurrent
+
+serve :: PortNumber -> IO ()
+serve port = withSocketsDo $ do
+    sock <- listenOn $ PortNumber port
+    loop sock
+
+loop sock = do
+    (conn, _) <- accept sock
+
+    -- Print incoming request information
+    req <- recv conn 4096
+    peer <- getPeerName conn
+    Prelude.putStrLn $ show peer ++ ": " ++ show req
+
+    forkIO $ body conn
+    loop sock
+    where
+        body c = do
+            sendAll c msg
+            sClose c
+
+httpify :: ByteString -> ByteString
+httpify content =
+    intercalate "\r\n" [
+          "HTTP/1.0 200 PISS OFF"
+        , "Content-Length: " `append` (pack $ show $ length content)
+        , ""
+        , content]
+
+msg :: ByteString
+msg = httpify "Piss off."
